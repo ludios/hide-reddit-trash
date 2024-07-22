731159e0e52401b711c9d13641ec5c90d3ab9c45
Ivan Kozik <ivan@ludios.org>
2024-07-22 03:10:59 +0000

    When another forecast is clicked, don't reset back to HRRR

--- hrrr_windy.user.js
+++ hrrr_windy.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        HRRR Windy
-// @description Always click 'HRRR' on windy.com to show 3km HRRR forecast
+// @description Default to 'HRRR' on windy.com to show 3km HRRR forecast
 // @namespace   ludios
 // @author      ludios
 // @license     AGPL-3.0; https://www.gnu.org/licenses/agpl-3.0.en.html
@@ -16,11 +16,8 @@ function queryElements(selector, callback) {
 }
 
 const logPrefix = "HRRR Windy: ";
-let pageCounter = 0;
 let mutations = 0;
 let observer = null;
-let lastHref = location.href;
-let locationInterval = null;
 
 // Observe some selectors and run a callback for each selected element.
 function observe(maxMutations, selectors, callback) {
@@ -53,61 +50,6 @@ function observe(maxMutations, selectors, callback) {
   }
 
   reobserve();
-
-  function navigated() {
-    console.log(`${logPrefix}navigated, resetting alreadyClicked and MutationObserver`);
-    observer.disconnect();
-    resetAlreadyClicked();
-    mutations = 0;
-    pageCounter = 0;
-    reobserve();
-  }
-
-  function startPollingLocation() {
-    if (locationInterval !== null) {
-      throw new Error(`locationInterval already exists: ${locationInterval}`);
-    }
-    locationInterval = setInterval(
-      () => {
-        if (location.href !== lastHref) {
-          lastHref = location.href;
-          navigated();
-        }
-      },
-      1000
-    );
-  }
-
-  function stopPollingLocation() {
-    if (locationInterval === null) {
-      throw new Error(`locationInterval === null`);
-    }
-    clearInterval(locationInterval);
-    locationInterval = null;
-  }
-
-  // So far, only Chromium-based browsers have Navigation API: https://caniuse.com/mdn-api_navigation
-  if (window.navigation && navigation.addEventListener) {
-    console.log(`${logPrefix}using Navigation API to detect location changes`);
-    navigation.addEventListener('navigate', (_ev) => {
-      navigated();
-    });
-  } else {
-    // https://stackoverflow.com/questions/34999976/detect-changes-on-the-url
-    // suggests that setInterval is the best way to do it when we lack Navigation API.
-    console.log(`${logPrefix}using setInterval(..., 1000) to detect location changes`);
-    startPollingLocation();
-    // Not necessary for correctness, but to save power, kill our setInterval when the page isn't visible.
-    document.addEventListener("visibilitychange", function() {
-      if (document.hidden) {
-        console.log(`${logPrefix}page has become hidden, stopping setInterval`);
-        stopPollingLocation();
-      } else {
-        console.log(`${logPrefix}page has become visible, starting setInterval`);
-        startPollingLocation();
-      }
-    });
-  }
 }
 
 let alreadyClicked;
