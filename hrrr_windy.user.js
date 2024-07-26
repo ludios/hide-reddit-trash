bb0a3cd7222a997a2d95dce0819b84943bd63e23
Ivan Kozik <ivan@ludios.org>
2024-07-26 05:06:23 +0000

    Run just on https://www.windy.com/* instead of every site

--- hrrr_windy.user.js
+++ hrrr_windy.user.js
@@ -6,6 +6,7 @@
 // @license     AGPL-3.0; https://www.gnu.org/licenses/agpl-3.0.en.html
 // @grant       none
 // @version     1.0.0
+// @match       https://www.windy.com/*
 // ==/UserScript==
 
 // These functions were copied from Expand Everything
